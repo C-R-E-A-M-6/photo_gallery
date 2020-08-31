59ce46e9be7d56de79ec7f280ba73fc90f8a6f60
cho319 <cho319us@gmail.com>
2020-08-30 18:54:17 -0700

    Optimized function to write 10 million rooms data to a csv file
    * 55 sec for 1 mil data
    - After removed console.log in loop
    * 7 sec for 1 mil data
    * 33 sec for 5 mil data
    - After implemented node's drain event
    * 4 sec for 1 mil data
    * 17 sec for 5 mil data
    * 34 sec for 10 mil data

--- db/PostgreSQL/generateRooms.js
+++ db/PostgreSQL/generateRooms.js
@@ -2,42 +2,52 @@ const fs = require('fs');
 const csvWriter = require('csv-write-stream');
 const writer = csvWriter();
 
-// description
-let adjectives = ['Big', 'Beautiful', 'Comfortable', 'Cosy', 'Huge', 'Small', 'Homely'];
-// starRating
-let integer = [3, 4, 5];
-// superhost
-let boolean = [true, false];
-// location
-let cities = ['Los Angeles', 'San Diego', 'San Jose', 'San Francisco', 'Fresno', 'Sacramento', 'Long Beach', 'Oakland', 'Bakersfield', 'Anaheim', 'Santa Ana', 'Riverside', 'Stockton', 'Irvine'];
-
-
-const seedRoomsData = (entries) => {
-  return new Promise((resolve, reject) => {
-    // The 'error' event is emitted if an error occurred while writing or piping data
-    writer.on('error', function(error) {
-      reject(error);
-    });
-    // The 'finish' event is emitted after the writer.end() method has been called
-    writer.on('finish', function(data) {
-      resolve(data);
-    })
-    writer.pipe(fs.createWriteStream('rooms.csv'));
-    for (let i = 1; i <= entries; i++) {
-      writer.write({
-        roomID: i,
-        description: `${adjectives[i % adjectives.length]} ${cities[i % cities.length]} Home`,
-        starRating: integer[i % integer.length] + (i / 100),
-        reviewTotal: i * i % 300,
-        superhost: boolean[i % boolean.length],
-        location: `${cities[i % cities.length]}, CA United States`
-      });
-      console.log('i: ', i);
-    }
-    writer.end();
+const generateRooms = (i, callback) => {
+  // The 'error' event is emitted if an error occurred while writing or piping data
+  writer.on('error', function(error) {
+    console.log('Failed');
   });
+  // The 'finish' event is emitted after the stream.end() method has been called
+  writer.on('finish', function(data) {
+    console.log('Succeeded');
+  })
+  // All the data from readable goes into 'file.txt'.
+  writer.pipe(fs.createWriteStream(`rooms.csv`));
+
+  let adjectives = ['Big', 'Beautiful', 'Comfortable', 'Cosy', 'Huge', 'Small', 'Homely'];
+  let rate = [3, 4, 5];
+  let cities = ['Los Angeles', 'San Diego', 'San Jose', 'San Francisco', 'Fresno', 'Sacramento', 'Long Beach', 'Oakland', 'Bakersfield', 'Anaheim', 'Santa Ana', 'Riverside', 'Stockton', 'Irvine'];
+  let id = 1;
+
+  function write() {
+    let ok = true;
+    while (i > 0 && ok) {
+      i--;
+      const data = {
+        roomID: id++,
+        description: `${adjectives[i % adjectives.length]} ${cities[i % cities.length]} Home`,
+        starRating: rate[i % rate.length] + (i / 100),
+        reviewTotal: i * i % 300,
+        superhost: i % 11 === 0 ? true : false,
+        location: `${cities[i % cities.length]}, CA United States`
+      };
+      // Last time
+      if (i === 0) {
+        writer.write(data, callback);
+      } else {
+        // See if continue, or wait.
+        ok = writer.write(data);
+      }
+    }
+    if (i > 0) {
+      // If a call to stream.write(chunk) returns false, the 'drain' event will be emitted when it is appropriate to resume writing data to the stream.
+      // Had to stop early. Write some more once it drains.
+      writer.once('drain', write);
+    }
+  }
+  write();
 }
 
-seedRoomsData(1000000)
-  .then(() => {console.log('Succeeded')})
-  .catch(() => {console.log('Failed')});
+generateRooms(10000000, () => {
+  writer.end();
+})
