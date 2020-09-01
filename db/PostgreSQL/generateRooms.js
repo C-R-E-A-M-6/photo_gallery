11a714314e0a666c5b1b3e06a534d3b2015bf3a9
cho319 <cho319us@gmail.com>
2020-08-31 20:21:33 -0700

    fixed typo

--- db/PostgreSQL/generateRooms.js
+++ db/PostgreSQL/generateRooms.js
@@ -15,7 +15,7 @@ const generateRooms = (i, callback) => {
   writer.pipe(fs.createWriteStream(`rooms.csv`));
 
   const adjectives = ['Big', 'Beautiful', 'Comfortable', 'Cosy', 'Huge', 'Small', 'Homely'];
-  const rate = [3.00, 3.32, 3.89, 3.93, 4.00, 4.12, 4,30, 4.63, 4.91, 5];
+  const rate = [3, 3.32, 3.89, 3.93, 4, 4.12, 4.30, 4.63, 4.91, 5];
   const cities = ['Los Angeles', 'San Diego', 'San Jose', 'San Francisco', 'Fresno', 'Sacramento', 'Long Beach', 'Oakland', 'Bakersfield', 'Anaheim', 'Santa Ana', 'Riverside', 'Stockton', 'Irvine'];
   let id = 1;
 
