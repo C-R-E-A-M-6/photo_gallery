e3d0b96a19851928f443c4b905e4cded9cd178df
cho319 <cho319us@gmail.com>
2020-09-03 10:58:03 -0700

    Updated the path to csv file

--- db/PostgreSQL/schema.sql
+++ db/PostgreSQL/schema.sql
@@ -20,9 +20,9 @@ CREATE TABLE photos (
   roomID INT NOT NULL
 );
 
-COPY rooms (description, starRating, reviewTotal, superhost, location) FROM '/Users/harryho/Desktop/JSProject/HRSF129/SDC/gallery/db/CSV/rooms.csv' DELIMITER ',' CSV HEADER;
+COPY rooms (description, starRating, reviewTotal, superhost, location) FROM '/Users/harryho/Desktop/JSProject/HRSF129/SDC/gallery/db/PostgreSQL/rooms.csv' DELIMITER ',' CSV HEADER;
 
-COPY photos (imageURL, image_description, roomID) FROM '/Users/harryho/Desktop/JSProject/HRSF129/SDC/gallery/db/CSV/photos.csv' DELIMITER ',' CSV HEADER;
+COPY photos (imageURL, image_description, roomID) FROM '/Users/harryho/Desktop/JSProject/HRSF129/SDC/gallery/db/PostgreSQL/photos.csv' DELIMITER ',' CSV HEADER;
 
 CREATE INDEX photos_roomID_index ON photos (roomID);
 
