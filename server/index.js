fe3d8126edae0fc7e2468e6f6e48edb7ceb24a12
cho319 <cho319us@gmail.com>
2020-09-05 00:32:29 -0700

    Connected client, server and database; Able to render correct data

--- server/index.js
+++ server/index.js
@@ -1,31 +1,53 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
 const PORT = 3001;
 
 //import database
-const db = require('../db/index');
-//import query models
-let dbQueries = require('./models/properties');
+const db = require('../db/PostgreSQL/index');
 
 //send static files inside the public folder
 app.use('/rooms/:id', express.static(path.join(__dirname, '../public')));
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/properties/:id', (req, res) => {
-  let id = req.params.id;
-  dbQueries.getProperties(id, (err, data) => {
+// Get room info
+app.get('/rooms/:id/photogallery', (req, res) => {
+  // declare query params
+  let queryParams = [req.params.id];
+  // declare query string
+  let queryString = 'select * from rooms, photos where rooms.roomID = $1 and rooms.roomID = photos.roomID;'
+  db.query(queryString, queryParams, (err, data) => {
     if (err) {
-      res.status(400).send('error');
+      console.log("Failed to get room info from databases: ", err);
+      res.status(400).send(err);
     } else {
-      console.log('success')
-      res.status(200).send(data);
+      console.log("Succeeded to get room info from databases");
+      // construct images array
+      let imagesArr = [];
+      for (let i = 0; i < data.rows.length; i++) {
+        let image = {
+          imageID: data.rows[i].imageid,
+          imageURL: data.rows[i].imageurl,
+          image_description: data.rows[i].image_description
+        }
+        imagesArr.push(image);
+      }
+      // construct the output
+      let output = {
+        roomID: data.rows[0].roomid,
+        description: data.rows[0].description,
+        starRating: data.rows[0].starrating,
+        reviewTotal: data.rows[0].reviewtotal,
+        superhost: data.rows[0].superhost,
+        location: data.rows[0].location,
+        images: imagesArr
+      }
+      console.log(output)
+      res.status(200).send(output);
     }
   });
-});
-
-app.get('/hi', (req, res) => {
-  res.send('hello world')
 });
 
 app.listen(PORT, () => {
