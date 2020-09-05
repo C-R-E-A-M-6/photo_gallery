1c7721fe1897ff3286c097e79f3cc68bd0740980
cho319 <cho319us@gmail.com>
2020-09-05 11:32:24 -0700

    Updated schema for DELETE query
    - since Postgres does not support using join directly in a DELETE statement, in order to delete data from two tables, we can using two query statements or ON DELETE CASCADE
    - CASCADE specifies that when a referenced row is deleted, row(s) referencing it should be automatically deleted as well

--- db/PostgreSQL/schema.sql
+++ db/PostgreSQL/schema.sql
@@ -17,7 +17,7 @@ CREATE TABLE photos (
   imageID SERIAL PRIMARY KEY,
   imageURL VARCHAR NOT NULL,
   image_description VARCHAR(100) NOT NULL,
-  roomID INT NOT NULL
+  roomID INT REFERENCES rooms(roomID) ON DELETE CASCADE NOT NULL
 );
 
 COPY rooms (description, starRating, reviewTotal, superhost, location) FROM '/Users/harryho/Desktop/JSProject/HRSF129/SDC/gallery/db/PostgreSQL/rooms.csv' DELIMITER ',' CSV HEADER;
@@ -25,5 +25,3 @@ COPY rooms (description, starRating, reviewTotal, superhost, location) FROM '/Us
 COPY photos (imageURL, image_description, roomID) FROM '/Users/harryho/Desktop/JSProject/HRSF129/SDC/gallery/db/PostgreSQL/photos.csv' DELIMITER ',' CSV HEADER;
 
 CREATE INDEX photos_roomID_index ON photos (roomID);
-
-ALTER TABLE photos ADD CONSTRAINT photos_roomID_foreignkey FOREIGN KEY (roomID) REFERENCES rooms (roomID);
